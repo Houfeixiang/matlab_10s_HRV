f4e071e3f84d7de5dcb28342126bcaea1aba8cd4
Houfeixiang <45409278+Houfeixiang@users.noreply.github.com>
2018-12-10 15:02:36 +0800

    Create LFdividesHF.m

---
+++ LFdividesHF.m
@@ -0,0 +1,21 @@
+function out = LF(in)
+% computes power in low frequency range (0.04 - 0.15 Hz)
+%
+% IN: vector [1xN] of N R-R intervals (ms)
+%
+% OUT: LF
+%
+% Jakub Kuzilek, 2012
+%
+% Ref: Task force of The European Society of Cardiology and North American 
+%      Society of Pacing and Electrophysiology: Heart rate variability - 
+%      Standards of measurement, phzsiological interpretation, and clinical use
+
+fs = 1000;
+
+[freq, data, N] = psd_estimation(in, fs,'welch');
+
+df = fs/N; % length of PSD window
+
+out = 2*sum(data(freq <=0.15 & freq >= 0.04))*df; % df - numerical integration I need to add frequency increment
+
